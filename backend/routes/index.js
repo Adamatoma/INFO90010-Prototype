c7087bd6e08c8c9e6d13cfc9d7e2ad4a564ffb66
Yutong Pan <yutongp1@student.unimelb.edu.au>
2020-10-07 23:25:06 +1100

    first time submission

---
+++ backend/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+var user = [
+  {
+    email: 'abc@gmail.com', password: 'password'
+  }
+]
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.send('weoair');
+});
+
+router.post('/login', function (req, res) {
+  let result = user.find(user => user.email == req.body.email);
+  if (result) {
+    if (result.password == req.body.password) {
+      res.status(200).send({
+        message: "Successful login!!"
+      })
+    } else {
+      res.status(200).send({
+        message: "Password incorrect!!"
+      })
+    }
+  } else {
+    res.status(200).send({
+      message: "User not found!!"
+    })
+  }
+})
+
+module.exports = router;
